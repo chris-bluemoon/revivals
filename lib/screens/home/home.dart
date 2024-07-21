14418fb7d6e5c581478bc311089c158a2936c728
cjmilner73 <chris@blueemoon.sg>
2024-07-22 01:45:18 +0700

    Added horizontal listview

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -68,7 +68,36 @@ class _HomeState extends State<Home> {
             onPressed: () => Navigator.pushNamed(context, '/new_arrivals'),
             icon: Icon(Icons.abc)
           ),
+        Expanded(
+          child: ListView(
+            // This next line does the trick.
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160,
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ),
         ],
+        
         
       ),
     );
