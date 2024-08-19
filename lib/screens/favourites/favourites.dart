9a220f768b29d14bc042cab1dec4c264187dfe60
John Doe <johndoe@example.com>
2024-08-19 21:10:00 +0700

    added no favs yet

--- lib/screens/favourites/favourites.dart
+++ lib/screens/favourites/favourites.dart
@@ -15,57 +15,65 @@ class Favourites extends StatefulWidget {
 }
 
 class _FavouritesState extends State<Favourites> {
+  bool anyFavourites = true;
 
-@override
-initState() {
-
-  Provider.of<ItemStore>(context, listen: false).populateFavourites();
-  super.initState();
-}
+  @override
+  initState() {
+    Provider.of<ItemStore>(context, listen: false).populateFavourites();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (Provider.of<ItemStore>(context, listen: false).favourites.length > 0) {
+      anyFavourites = true;
+    } else {
+      anyFavourites = false;
+    }
     return Scaffold(
       appBar: AppBar(
         // centerTitle: true,
         title: const Row(
-          // TODO: Image is not centered in appbar with back arrow
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('WISH LIST', style: TextStyle(fontSize: 22))
-              ]),
+            // TODO: Image is not centered in appbar with back arrow
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('WISH LIST', style: TextStyle(fontSize: 22))]),
       ),
-      body: Container(
+      body: anyFavourites ? Container(
           color: Colors.white,
-          child: Column(
-            children: [
-              Consumer<ItemStore>(
-                  // child not required
-                  builder: (context, value, child) {
-                return Expanded(
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2, childAspectRatio: 0.5),
-                    itemBuilder: (_, index) =>
-                    GestureDetector(
-                        child: ItemCard(value.favourites[index]),
-                        onTap: () {
-                          // if (Provider.of<ItemStore>(context, listen: false).renters.length == 0) {
-                            // log('Not logged in, cannot rent, redirecting');
-                            // Navigator.of(context).push(MaterialPageRoute(builder: (context) => (GoogleSignInScreen())));
-                          // } else {
-                            // log('About to rent ${value.items[index].name}');
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => (ToRent(value.favourites[index]))));
-                          // }
-
-                        }),
-                    itemCount: value.favourites.length,
-                  ),
-                );
-              }),
-            ],
-          )),
+          child: Consumer<ItemStore>(
+              // child not required
+              builder: (context, value, child) {return 
+                GridView.builder(
+                  gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, childAspectRatio: 0.5),
+                  itemBuilder: (_, index) => GestureDetector(
+                      child: ItemCard(value.favourites[index]),
+                      onTap: () {
+                        // if (Provider.of<ItemStore>(context, listen: false).renters.length == 0) {
+                        // log('Not logged in, cannot rent, redirecting');
+                        // Navigator.of(context).push(MaterialPageRoute(builder: (context) => (GoogleSignInScreen())));
+                        // } else {
+                        // log('About to rent ${value.items[index].name}');
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                (ToRent(value.favourites[index]))));
+                        // }
+                      }),
+                  itemCount: value.favourites.length,
+    );})
+    ) : Container(
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('No favourites yet!'),
+          ],
+        )
+      ],)
+    )
     );
   }
 }
